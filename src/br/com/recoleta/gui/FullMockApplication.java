3dc60baf301127ef334d600593c0a5f17e8c984f
Vinicius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2023-11-14 11:06:20 -0300

    clean code

--- src/br/com/recoleta/gui/FullMockApplication.java
+++ src/br/com/recoleta/gui/FullMockApplication.java
@@ -8,25 +8,16 @@ import javax.swing.SwingUtilities;
 import br.com.recoleta.model.User;
 
 public class FullMockApplication {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                List<User> mockUserDatabase = new ArrayList<>();
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				List<User> mockUserDatabase = new ArrayList<>();
 
-                
-                UserLoginForm loginForm = new UserLoginForm(mockUserDatabase);
-                /*UserRegistrationForm registrationForm = new UserRegistrationForm(mockUserDatabase);
-                CollectionForm collectionForm = new CollectionForm(); // If applicable
-                AddressSetupForm addressSetupForm = new AddressSetupForm(); // If applicable*/
+				UserLoginForm loginForm = new UserLoginForm(mockUserDatabase);
+				loginForm.setVisible(true);
 
-                // You can manage the flow of your application, e.g., show login form initially
-                loginForm.setVisible(true);
-               /* registrationForm.setVisible(false);
-                collectionForm.setVisible(false);
-                addressSetupForm.setVisible(false);*/
-            }
-        });
-    }
+			}
+		});
+	}
 }
-
